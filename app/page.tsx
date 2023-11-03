78d79214a6994187cb3e281392d6c4746e40f015
zoutigo <zoutigo@gmail.com>
2023-11-03 19:34:54 +0100

    dev deploy test

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Je veux que ça marche bie n &nbsp;
+          Je veux que ça marche bien. heureux !! &nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
         <p>
